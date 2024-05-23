f9521fa2b26bfd30b6a942dbb7526b7fd60e745a
elmin412 <115942958+elmin412@users.noreply.github.com>
2024-05-23 18:58:22 +0200

    fix

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -68,23 +68,3 @@ export const refreshUser = createAsyncThunk(
     }}
 )
  
-
-// export const refreshUser = createAsyncThunk(
-//     'auth/refresh',
-//     async (_, thunkAPI) => {
-//         const state = thunkAPI.getState();
-//         const persistedToken = state.auth.token;
-
-//         if (persistedToken === null) {
-//             return thunkAPI.rejectWithValue('Unable to fetch user');
-//         }
-//         try {
-//         setAuthHeader(persistedToken);
-//             const response = await axios.get('users/current');
-//             return response.data
-//     } catch (error) {
-//         return thunkAPI.rejectWithValue(error.message);
-//     }
-//         }
-// )
- 
